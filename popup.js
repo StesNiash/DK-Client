6b9fe5f00254007e7d84cbe7cd8f631bf56164d2
StesNiash <111519914+StesNiash@users.noreply.github.com>
2025-05-22 19:57:55 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -377,7 +377,7 @@ function populatePairSelect(asset) {
   }
 }
 
-const SERVER_URL = "https://your-server.com/api/login"; // Замените на ваш адрес
+const SERVER_URL = "https://176.108.253.203/login"; // Замените на ваш адрес
 
 function showLogin() {
   document.getElementById("loginSection").style.display = "";
